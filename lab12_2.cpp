ee764897b4bafa54cacabc8727e415816654809f
krit <krittapasboonruang@gmail.com>
2026-01-06 20:54:29 +0700

    lab12_2 completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
-
+int gcd(int a, int b);
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+int gcd(int a, int b){
+
+    if(b == 0){
+        return a;
+    }
+
+    else{
+        return gcd(b, a % b);
+    }
 }
